eee02b6d8696a056acd99eb0023f1b1a817441f2
DaviUgioni <davi.395914@alunosatc.edu.br>
2026-06-23 20:28:50 -0300

    a
    a

--- MixTudo.js
+++ MixTudo.js
@@ -1,13 +1,12 @@
-
-const elements = document.querySelectorAll('.mix-card, .mix-about, .mix-title');
+const elements = document.querySelectorAll('.mix-card-sobre, .mix-about, .mix-title, .mix-produto');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if(entry.isIntersecting){
+    if (entry.isIntersecting) {
       entry.target.classList.add('show');
     }
   });
-});
+}, { threshold: 0.15 });
 
 elements.forEach(el => {
   el.classList.add('hidden');
